1dd9fd874b078e025610e0cf6a5fbfb6a6b20ace
Colin Willoughby <39289465+colinwillowtree@users.noreply.github.com>
2025-01-14 15:11:32 -0800

    Add files via upload

---
+++ P5_Images.js
@@ -0,0 +1,25 @@
+let robot = [];
+
+function preload() {
+  for (var i = 0; i < 48; i++) {
+    robot[i] = loadImage('data/robot/robot_' + nf(i, 4) + '.png');
+  }
+  
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  imageMode(CENTER);
+  frameRate(30);
+}
+
+
+function draw() {
+  background(31);
+  image(robot[frameCount%48], width/2, height/2, height, height);
+  
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
